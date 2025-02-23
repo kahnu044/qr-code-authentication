622647685a5026bebe08cbcd29590b97eb25806b
kahnu044 <kahnucharan044@gmail.com>
2025-02-23 20:12:13 +0530

    Pusher setup done

--- qr-code-auth-backend-server/server.js
+++ qr-code-auth-backend-server/server.js
@@ -1,8 +1,18 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const Pusher = require("pusher");
 const app = express();
 const PORT = process.env.PORT || 5002;
+
+//Pusher configuration
+const pusher = new Pusher({
+  appId: process.env.PUSHER_APP_ID,
+  key: process.env.PUSHER_APP_KEY,
+  secret: process.env.PUSHER_APP_SECRET,
+  cluster: process.env.PUSHER_APP_CLUSTER,
+  useTLS: true,
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
