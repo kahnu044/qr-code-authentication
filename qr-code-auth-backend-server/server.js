4f52e2a9f5261db99b961f171d2491cf0a5e2517
Kahnu Charan Swain <kahnuc.swain@nettantra.net>
2025-02-24 16:07:02 +0530

    MongoDB Config Updated

--- qr-code-auth-backend-server/server.js
+++ qr-code-auth-backend-server/server.js
@@ -1,26 +1,20 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const authRoutes = require("./routes/authRoutes");
 const pusherRoutes = require("./routes/pusherRoutes");
+const connectDB = require('./config/db');
 
 const app = express();
 const PORT = process.env.PORT || 5002;
 
-// Connect to MongoDB (adjust the connection string as needed)
-mongoose.connect("mongodb://localhost:27017/qr-auth", {});
-mongoose.connection.on("connected", () => {
-  console.log("Connected to MongoDB");
-});
-mongoose.connection.on("error", (err) => {
-  console.error("MongoDB connection error:", err);
-});
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+
+// Connect to MongoDB
+connectDB();
 
 // Routes
 app.use("/auth", authRoutes);
