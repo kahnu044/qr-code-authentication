795d1f274fb8b4a98c5c08b4e08e8968e5f8d3de
Kahnu Charan Swain <kahnuc.swain@nettantra.net>
2025-02-24 11:50:16 +0530

    User Schema Added

---
+++ qr-code-auth-backend-server/models/User.js
@@ -0,0 +1,23 @@
+// models/User.js
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  name: { type: String }, // Optional display name
+});
+
+// Hash the password before saving if it is new or modified
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    return next();
+  } catch (error) {
+    return next(error);
+  }
+});
+
+module.exports = model("User", userSchema);
