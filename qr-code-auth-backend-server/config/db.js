8b4e651a35d4934bcaeb6490784be911f1d0e32d
Kahnu Charan Swain <kahnuc.swain@nettantra.net>
2025-02-24 16:09:50 +0530

    MONGO_URI updated

--- qr-code-auth-backend-server/config/db.js
+++ qr-code-auth-backend-server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    mongoose.connect("mongodb://localhost:27017/qr-auth", {});
+    mongoose.connect(process.env.MONGO_URI, {});
     console.log("MongoDB connected");
   } catch (err) {
     console.error("MongoDB connection error:", err);
