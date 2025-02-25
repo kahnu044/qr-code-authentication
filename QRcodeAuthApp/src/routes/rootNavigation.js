6597bde0dbfab8fc2375efca007c80e113595b6e
kahnu044 <kahnucharan044@gmail.com>
2025-02-25 16:31:18 +0530

    Vector icons Added

--- QRcodeAuthApp/src/routes/rootNavigation.js
+++ QRcodeAuthApp/src/routes/rootNavigation.js
@@ -5,6 +5,7 @@ import LoginScreen from '../screens/LoginScreen';
 import HomeScreen from '../screens/HomeScreen';
 import SessionScreen from '../screens/SessionScreen';
 import AccountScreen from '../screens/AccountScreen';
+import Icon from 'react-native-vector-icons/Feather';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -16,17 +17,32 @@ function TabNavigator() {
       <Tab.Screen
         name="Home"
         component={HomeScreen}
-        options={{headerShown: false}}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({color, size, focused, tintColor}: any) => {
+            return <Icon name="home" color="#F3C81DFF" size={20} />;
+          },
+        }}
       />
       <Tab.Screen
         name="Session"
         component={SessionScreen}
-        options={{headerShown: false}}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({color, size, focused, tintColor}: any) => {
+            return <Icon name="info" color="#F3C81DFF" size={20} />;
+          },
+        }}
       />
       <Tab.Screen
         name="Account"
         component={AccountScreen}
-        options={{headerShown: false}}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({color, size, focused, tintColor}: any) => {
+            return <Icon name="user" color="#F3C81DFF" size={20} />;
+          },
+        }}
       />
     </Tab.Navigator>
   );
