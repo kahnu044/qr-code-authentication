86cecc0b8ed108191b01f62a637ea8a86f115cda
kahnu044 <kahnucharan044@gmail.com>
2025-02-26 20:55:34 +0530

    HomeScreen Updated

--- QRcodeAuthApp/src/screens/HomeScreen.jsx
+++ QRcodeAuthApp/src/screens/HomeScreen.jsx
@@ -4,13 +4,33 @@ import React from 'react';
 
 const HomeScreen = () => {
   console.log('Test fPI Path', process.env.API_PATH);
+  let primaryColor = '#366cf2';
+  let userName = 'Kahnu';
+
   return (
     <SafeAreaView
       style={{flex: 1, backgroundColor: '#ffffff', paddingHorizontal: 12}}>
       <ScrollView>
         <View>
-          <Text>HomeScreen</Text>
+          <Text style={{color: primaryColor, fontWeight: 'bold', fontSize: 35}}>
+            Hi, {userName} 👋
+          </Text>
         </View>
+
+        {/* <View
+          style={{
+            backgroundColor: 'white',
+            height: 60,
+            padding: 8,
+            borderRadius: 10,
+            elevation: 10,
+            shadowColor: '#000',
+            marginTop: 10,
+          }}>
+          <Text style={{color: primaryColor, fontWeight: 'bold', fontSize: 20}}>
+            Session - 1, from Chrome - Mac OS
+          </Text>
+        </View> */}
       </ScrollView>
     </SafeAreaView>
   );
