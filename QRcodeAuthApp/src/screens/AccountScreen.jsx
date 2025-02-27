c59c7732f1c3b6a2065a5e0931abe422158060e7
kahnu044 <kahnucharan044@gmail.com>
2025-02-28 00:00:09 +0530

    Logout button added

--- QRcodeAuthApp/src/screens/AccountScreen.jsx
+++ QRcodeAuthApp/src/screens/AccountScreen.jsx
@@ -1,14 +1,36 @@
-import {StyleSheet, Text, View, ScrollView} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native';
 import React from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {useNavigation} from '@react-navigation/native';
 
 const AccountScreen = () => {
+  const navigation = useNavigation();
+  const handleLogout = () => {
+    AsyncStorage.removeItem('authToken');
+    navigation.replace('Login');
+  };
   return (
     <SafeAreaView
       style={{flex: 1, backgroundColor: '#ffffff', paddingHorizontal: 12}}>
       <ScrollView>
         <View>
           <Text>AccountScreen</Text>
+          <TouchableOpacity
+            activeOpacity={0.9}
+            onPress={handleLogout}
+            style={{
+              backgroundColor: 'red',
+              text: '#FFF',
+            }}>
+            <Text>logout</Text>
+          </TouchableOpacity>
         </View>
       </ScrollView>
     </SafeAreaView>
