70499c27e47b436d3b60944bb7929e8e7a00ddc3
kahnu044 <kahnucharan044@gmail.com>
2025-02-28 09:35:00 +0530

    handle auth

--- QRcodeAuthApp/App.tsx
+++ QRcodeAuthApp/App.tsx
@@ -1,8 +1,18 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import RootNavigation from './src/routes/rootNavigation';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const home = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      const token = await AsyncStorage.getItem('authToken');
+      setIsLoggedIn(token ? true : false);
+    };
+    checkAuth();
+  }, []);
+
   return <RootNavigation isLoggedIn={isLoggedIn} />;
 };
 export default home;
