3e2d785870fcdac2fa3944aa938cc2df72ac852c
kahnu044 <kahnucharan044@gmail.com>
2025-02-28 10:47:18 +0530

    Session list added

--- QRcodeAuthApp/src/screens/HomeScreen.jsx
+++ QRcodeAuthApp/src/screens/HomeScreen.jsx
@@ -1,5 +1,6 @@
 import {StyleSheet, Text, View, ScrollView} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import React from 'react';
 
 const HomeScreen = () => {
@@ -17,20 +18,32 @@ const HomeScreen = () => {
           </Text>
         </View>
 
-        {/* <View
+        <View
           style={{
-            backgroundColor: 'white',
-            height: 60,
-            padding: 8,
+            backgroundColor: '#F4F4F4',
+            padding: 10,
             borderRadius: 10,
-            elevation: 10,
-            shadowColor: '#000',
             marginTop: 10,
           }}>
-          <Text style={{color: primaryColor, fontWeight: 'bold', fontSize: 20}}>
-            Session - 1, from Chrome - Mac OS
-          </Text>
-        </View> */}
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+            }}>
+            <View style={{flexDirection: 'column', gap: 5}}>
+              <Text
+                style={{color: '#000000', fontWeight: 'bold', fontSize: 16}}>
+                Session - 1, from Chrome - Mac OS
+              </Text>
+              <Text
+                style={{color: '#B2B0B0', fontWeight: 'bold', fontSize: 14}}>
+                Last seen : 10:00 AM
+              </Text>
+            </View>
+            <Icon name="logout" size={30} color={'#000'} />
+          </View>
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
